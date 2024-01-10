0a03fa7be980c35b2c1d355e9ec7730f357c2b56
noproblem2311 <quanpva.dev@gmail.com>
2024-01-10 13:29:32 +0700

    first commit

---
+++ EDUHUNT_BE/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EDUHUNT_BE.Data
+{
+    public class AppDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser>(options)
+    {
+
+    }
+}
